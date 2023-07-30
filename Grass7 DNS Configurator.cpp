7e9bed6c0840e8b139a12e2807ddf063bef14d28
Genki <86833542+Genki-jp0@users.noreply.github.com>
2023-07-30 14:58:01 +0200

    Update Grass7 DNS Configurator.cpp

--- Grass7 DNS Configurator.cpp
+++ Grass7 DNS Configurator.cpp
@@ -7,113 +7,16 @@
 #include <string>
 #include <Gr7API.h>
 
-// We do this horribleness
-int InfoQueryFailed(void)
+int InfoTaskFail(UINT uID, PCWSTR pszIcon)
 {
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_QUERY_FAIL), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
+	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandleW(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandleW(NULL), uID), TDCBF_CLOSE_BUTTON, pszIcon, NULL);
 	exit(0);
-}
-
-int InfoServiceAlreadyRunning(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_ALREADY_RUNNING), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	exit(0);
-}
-
-int InfoServiceAlreadyStopped(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_ALREADY_STOPPED), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	exit(0);
-}
-
-int InfoServiceStartFail(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_START_FAIL), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceStartSuccess(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_START_SUCCESS), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceStopFail(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_STOP_FAIL), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceStopSuccess(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_STOP_SUCCESS), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceEnableFail(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_ENABLE_FAIL), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
-	return 0;
-} 
-
-int InfoServiceEnableSuccess(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_ENABLE_SUCCESS), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceDisableFail(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_DISABLE_FAIL), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceDisableSuccess(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_DISABLE_SUCCESS), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceAlreadyEnabled(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_ALREADY_ENABLED), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceAlreadyDisabled(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_ALREADY_DISABLED), TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceConfigGetFail(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_CONFIG_GET_FAIL), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
-	return 0;
-}
-
-int InfoQueryServiceConfigFail(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_CONFIG_QUERY_FAIL), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceDisabled(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_DISABLED), TDCBF_CLOSE_BUTTON, TD_WARNING_ICON, NULL);
-	return 0;
-}
-
-int InfoServiceNoExist(void)
-{
-	TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_SERVICE_NO_EXIST), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
-	exit (0);
 }
 
 int main(void)
 {
 	if(gr7::isProcessElevated(GetCurrentProcess()) != 1) {
-		TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandle(NULL), IDS_UAC_ERROR_TEXT), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
+		TaskDialog(NULL, NULL, gr7::LoadOSBrandingString(), gr7::LoadStringToW(GetModuleHandleW(NULL), IDS_INFO_HEADER), gr7::LoadStringToW(GetModuleHandleW(NULL), IDS_UAC_ERROR_TEXT), TDCBF_CLOSE_BUTTON, TD_ERROR_ICON, NULL);
 		exit(0);
 	}
 
@@ -126,20 +29,21 @@ int main(void)
 	DWORD cbBufSize;
 	DWORD dwError;
 
-	SCManager = OpenSCManager(NULL, SERVICES_ACTIVE_DATABASE, SC_MANAGER_ALL_ACCESS);
-	SCService = OpenService(SCManager, _T("Unbound"), SERVICE_ALL_ACCESS);
+	SCManager = OpenSCManagerW(NULL, SERVICES_ACTIVE_DATABASE, SC_MANAGER_ALL_ACCESS);
+	SCService = OpenServiceW(SCManager, _T("Unbound"), SERVICE_ALL_ACCESS);
 
 	if(! QueryServiceStatusEx(SCService, SC_STATUS_PROCESS_INFO, (LPBYTE) &ssp, sizeof(SERVICE_STATUS_PROCESS), &dwBytesNeeded))
 	{
 		int errorCode = GetLastError();
-		if(errorCode == 6) {
-			InfoServiceNoExist();
-		} else {
-		InfoQueryFailed();
+		if (errorCode == 6) {
+			InfoTaskFail(IDS_INFO_SERVICE_NO_EXIST, TD_ERROR_ICON);
+		}
+		else {
+			InfoTaskFail(IDS_INFO_QUERY_FAIL, TD_ERROR_ICON);
 		}
 	}
 
-	if(! QueryServiceConfig(SCService, NULL, 0, &dwBytesNeeded2))
+	if(! QueryServiceConfigW(SCService, NULL, 0, &dwBytesNeeded2))
 	{
 		dwError = GetLastError();
 		if(ERROR_INSUFFICIENT_BUFFER == dwError)
@@ -147,29 +51,28 @@ int main(void)
 			cbBufSize = dwBytesNeeded2;
 			lpsc = (LPQUERY_SERVICE_CONFIG) LocalAlloc(LMEM_FIXED, cbBufSize);
 		} else {
-			InfoServiceConfigGetFail();
-			exit(0);
+			InfoTaskFail(IDS_INFO_CONFIG_GET_FAIL, TD_ERROR_ICON);
 		}
 	}
 
-	if(! QueryServiceConfig(SCService, lpsc, cbBufSize, &dwBytesNeeded2))
+	if(! QueryServiceConfigW(SCService, lpsc, cbBufSize, &dwBytesNeeded2))
 	{
-		InfoQueryServiceConfigFail();
+		InfoTaskFail(IDS_INFO_CONFIG_QUERY_FAIL, TD_ERROR_ICON);
 	}
 
 	HRESULT hr;
 	TASKDIALOGCONFIG tdc = { sizeof(TASKDIALOGCONFIG) };
 	int nClickedBtn;
-	HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MAIN_ICON));
+	HICON hIcon = LoadIconW(GetModuleHandleW(NULL), MAKEINTRESOURCE(IDI_MAIN_ICON));
 	LPCWSTR szTitle = gr7::LoadOSBrandingString(),
-	  szHeader = gr7::LoadStringToW(GetModuleHandle(NULL), IDS_HEADER),
-	  szBodyText = gr7::LoadStringToW(GetModuleHandle(NULL), IDS_BODY_TEXT);
+	  szHeader = gr7::LoadStringToW(GetModuleHandleW(NULL), IDS_HEADER),
+	  szBodyText = gr7::LoadStringToW(GetModuleHandleW(NULL), IDS_BODY_TEXT);
 	TASKDIALOG_BUTTON aCustomButtons[] = {
-	  { 1000, gr7::LoadStringToW(GetModuleHandle(NULL),IDS_BUTTON_START) },
-	  { 1001, gr7::LoadStringToW(GetModuleHandle(NULL),IDS_BUTTON_STOP) },
-	  { 1002, gr7::LoadStringToW(GetModuleHandle(NULL),IDS_BUTTON_ENABLE) },
-	  { 1003, gr7::LoadStringToW(GetModuleHandle(NULL),IDS_BUTTON_DISABLE) },
-	  { 1004, gr7::LoadStringToW(GetModuleHandle(NULL),IDS_BUTTON_EXIT) }
+	  { 1000, gr7::LoadStringToW(GetModuleHandleW(NULL),IDS_BUTTON_START) },
+	  { 1001, gr7::LoadStringToW(GetModuleHandleW(NULL),IDS_BUTTON_STOP) },
+	  { 1002, gr7::LoadStringToW(GetModuleHandleW(NULL),IDS_BUTTON_ENABLE) },
+	  { 1003, gr7::LoadStringToW(GetModuleHandleW(NULL),IDS_BUTTON_DISABLE) },
+	  { 1004, gr7::LoadStringToW(GetModuleHandleW(NULL),IDS_BUTTON_EXIT) }
 	};
  
 	  tdc.hwndParent = NULL;
@@ -188,52 +91,50 @@ int main(void)
 		{
 			if(lpsc->dwStartType == SERVICE_DISABLED)
 			{
-				InfoServiceDisabled();
-				exit(0);
+				InfoTaskFail(IDS_INFO_DISABLED,TD_WARNING_ICON);
 			}
 			if(ssp.dwCurrentState != SERVICE_STOPPED && ssp.dwCurrentState != SERVICE_STOP_PENDING)
 			{
-				InfoServiceAlreadyRunning();
+				InfoTaskFail(IDS_INFO_ALREADY_RUNNING, TD_INFORMATION_ICON);
 			}
 			if(! StartService(SCService, 0, NULL))
 			{
-				InfoServiceStartFail();
+				InfoTaskFail(IDS_INFO_START_FAIL, TD_ERROR_ICON);
 			} else {
-				InfoServiceStartSuccess();
+				InfoTaskFail(IDS_INFO_START_SUCCESS, TD_INFORMATION_ICON);
 			}
 		}
 	  if ( SUCCEEDED(hr) && 1001 == nClickedBtn )
 		{
 			if(ssp.dwCurrentState == SERVICE_STOPPED)
 			{
-				InfoServiceAlreadyStopped();
+				InfoTaskFail(IDS_INFO_ALREADY_STOPPED, TD_INFORMATION_ICON);
 			}
 			if(! ControlService(SCService, SERVICE_CONTROL_STOP, (LPSERVICE_STATUS) &ssp))
 			{
-				InfoServiceStopFail();
+				InfoTaskFail(IDS_INFO_STOP_FAIL, TD_INFORMATION_ICON);
 			} else {
-				InfoServiceStopSuccess();
+				InfoTaskFail(IDS_INFO_STOP_SUCCESS, TD_INFORMATION_ICON);
 			}
 		}
 	  if ( SUCCEEDED(hr) && 1002 == nClickedBtn )
 		{
 			if(lpsc->dwStartType == SERVICE_AUTO_START)
 			{
-				InfoServiceAlreadyEnabled();
-				exit(0);
+				InfoTaskFail(IDS_INFO_ALREADY_ENABLED, TD_INFORMATION_ICON);
 			}
-			if(! ChangeServiceConfig(SCService, SERVICE_NO_CHANGE, SERVICE_AUTO_START, SERVICE_NO_CHANGE, NULL, NULL, NULL, NULL, NULL, NULL, NULL))
+			if(! ChangeServiceConfigW(SCService, SERVICE_NO_CHANGE, SERVICE_AUTO_START, SERVICE_NO_CHANGE, NULL, NULL, NULL, NULL, NULL, NULL, NULL))
 			{
-				InfoServiceEnableFail();
+				InfoTaskFail(IDS_INFO_ENABLE_FAIL, TD_ERROR_ICON);
 			} else {
-			InfoServiceEnableSuccess();
+				InfoTaskFail(IDS_INFO_ENABLE_SUCCESS, TD_INFORMATION_ICON);
 			}
-			ShellExecute(NULL, NULL, L"cmd", L"/c %SystemRoot%\\System32\\dnssetting1.bat", NULL, SW_HIDE);
+			ShellExecuteW(NULL, NULL, L"cmd", L"/c %SystemRoot%\\System32\\dnssetting1.bat", NULL, SW_HIDE);
 			if(ssp.dwCurrentState == SERVICE_STOPPED)
 			{
-				if(! StartService(SCService, 0, NULL))
+				if(! StartServiceW(SCService, 0, NULL))
 				{
-					InfoServiceStartFail();
+					InfoTaskFail(IDS_INFO_START_FAIL, TD_ERROR_ICON);
 				}
 			}
 		}
@@ -241,22 +142,22 @@ int main(void)
 		{
 			if(lpsc->dwStartType == SERVICE_DISABLED)
 			{
-				InfoServiceAlreadyDisabled();
+				InfoTaskFail(IDS_INFO_ALREADY_DISABLED, TD_INFORMATION_ICON);
 				exit(0);
 			}
 			if(ssp.dwCurrentState != SERVICE_STOPPED && ssp.dwCurrentState != SERVICE_STOP_PENDING)
 			{
 				if(! ControlService(SCService, SERVICE_CONTROL_STOP, (LPSERVICE_STATUS) &ssp))
 				{
-					InfoServiceStopFail();
+					InfoTaskFail(IDS_INFO_STOP_FAIL, TD_ERROR_ICON);
 				}
 			}
-			ShellExecute(NULL, NULL, L"cmd", L"/c %SystemRoot%\\System32\\dnssetting2.bat", NULL, SW_HIDE);
-			if(! ChangeServiceConfig(SCService, SERVICE_NO_CHANGE, SERVICE_DISABLED, SERVICE_NO_CHANGE, NULL, NULL, NULL, NULL, NULL, NULL, NULL))
+			ShellExecuteW(NULL, NULL, L"cmd", L"/c %SystemRoot%\\System32\\dnssetting2.bat", NULL, SW_HIDE);
+			if(! ChangeServiceConfigW(SCService, SERVICE_NO_CHANGE, SERVICE_DISABLED, SERVICE_NO_CHANGE, NULL, NULL, NULL, NULL, NULL, NULL, NULL))
 			{
-				InfoServiceDisableFail();
+				InfoTaskFail(IDS_INFO_DISABLE_FAIL, TD_ERROR_ICON);
 			} else {
-			InfoServiceDisableSuccess();
+				InfoTaskFail(IDS_INFO_DISABLE_SUCCESS, TD_INFORMATION_ICON);
 			}
 		}
 	  CloseServiceHandle(SCService);
